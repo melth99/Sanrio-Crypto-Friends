2bcbafc5fff00cbf83cfc31a37ff316b78693bcc
mel-th <meleyth@gmail.com>
2025-03-25 19:21:44 -0400

    corrected live request data

--- services.js
+++ services.js
@@ -1,7 +1,7 @@
 const { randomBytes } = require("crypto")
 const dotenv = require("dotenv")
 const { captureRejectionSymbol } = require("events")
-dotenv.config({path:'.env'})
+dotenv.config({ path: '.env' })
 const baseURL = 'http://api.coinlayer.com/'
 
 // https://coinlayer.com/documentation
@@ -35,7 +35,7 @@ async function fetchConvert() {
     try {
 
         const response = await fetch(`${baseURL}${endpoint}?access_key=${process.env.ACCESS_KEY}&from=${from}&to=${to}&amount=${amount}`)
-        if (!response.ok){
+        if (!response.ok) {
             throw new Error(`Response status: ${response.status}`)
         }
         const json = await response.json()
@@ -47,38 +47,45 @@ async function fetchConvert() {
 
 }
 // await fetch(`${baseURL}${endpoint}?access_key=${process.env.ACCESS_KEY}&expand=1`)//
-async function fetchList() { //default value null
+async function fetchList(coinSymbol = null) { //default value null
     const endpoint = 'list'
-    
+
     try {
-        
-        const response = await fetch(`${baseURL}${endpoint}?access_key=${process.env.ACCESS_KEY}`)
+
+        const response = await fetch(`${baseURL}${endpoint}?access_key=${process.env.ACCESS_KEY}&extend=1`)
         if (!response.ok) {
             throw new Error(`Response status: ${response.status}`);
-            }
-        
+        }
+
         const json = await response.json()
         //console.log(json) //prints all currency data as wll as each cryptocurrency
         //console.log(json.fiat)//prints currency fiat refers to value in traditional currency
         //console.log(json.fiat.NAD) // returns full name of currency using short hand as a key in json
-        //console.log(json.crypto.ACP.max_supply) // different for fetch live.
-    
+        //console.log(json.crypto[coinSymbol]) // different for fetch live.
+
+        if (coinSymbol) { // specific coin
+            console.log(json.crypto[coinSymbol])
+        }
+        else {  
+            console.log(json.crypto)
+        }
     } catch (err) {
         console.error(err)
-    }}
+    }
+}
 
 
 
 async function fetchLiveData() {
     //extended results includes
-/*    timestamp -exact utc timestamp
-    default target for specified currency
-    exchange rate
-    high/low ER- highest/lowest midpoint exchange rate on that day 
-    volume - volume of cryptocurrency exchanged on requested date
-    market cap - total value of crypto currency
-
-    */
+    /*    timestamp -exact utc timestamp
+        default target for specified currency
+        exchange rate
+        high/low ER- highest/lowest midpoint exchange rate on that day 
+        volume - volume of cryptocurrency exchanged on requested date
+        market cap - total value of crypto currency
+    
+        */
     const endpoint = 'live' //added '?'
     //const symbol = input("What Cryptocurrency would you like (for now write 'btc'")
     try {
@@ -96,6 +103,7 @@ async function fetchLiveData() {
 }
 
 //fetchLiveDa()
-fetchList()
 //fetchHistory()
 //fetchConvert()
+const coinSymbol = 'ZSC'
+fetchList(coinSymbol)
